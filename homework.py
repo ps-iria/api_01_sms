dbe6e6b20ace2abe862e149ff39b498d97ec4333
drpsychps <68935465+drpsychps@users.noreply.github.com>
2020-11-24 17:23:58 +0700

    After review

--- homework.py
+++ homework.py
@@ -1,4 +1,6 @@
 import os
+from venv import logger
+
 import requests
 import time
 from dotenv import load_dotenv
@@ -6,29 +8,39 @@ from twilio.rest import Client
 
 load_dotenv()
 
+URL_USERS_GET = 'https://api.vk.com/method/users.get'
+VERSION_API = "5.126"
+VK_TOKEN = os.environ['VK_TOKEN']
+ACCOUNT_SID = os.environ['ACCOUNT_SID']
+AUTH_TOKEN = os.environ['AUTH_TOKEN']
+NUMBER_FROM = os.environ['NUMBER_FROM']
+NUMBER_TO = os.environ['NUMBER_TO']
+
+client = Client(ACCOUNT_SID, AUTH_TOKEN)
+
 
 def get_status(user_id):
     params = {
         "user_ids": user_id,
-        "v": "5.126",
-        "access_token": os.environ['VK_TOKEN'],
+        "v": VERSION_API,
+        "access_token": VK_TOKEN,
         "fields": "online"
     }
-    status = requests.post('https://api.vk.com/method/users.get',
-                           params=params)
-    status = status.json()["response"][0]["online"]
+    try:
+        status = requests.post(URL_USERS_GET,
+                               params=params)
+        status = status.json()["response"][0]["online"]
+    except Exception as e:
+        logger.exception(e)
+        status = e
     return status
 
 
 def sms_sender(sms_text):
-    account_sid = os.environ['ACCOUNT_SID']
-    auth_token = os.environ['AUTH_TOKEN']
-    client = Client(account_sid, auth_token)
-
     message = client.messages.create(
         body=sms_text,
-        from_=os.environ['NUMBER_FROM'],
-        to=os.environ['NUMBER_TO']
+        from_=NUMBER_FROM,
+        to=NUMBER_TO
     )
     return message.sid
 
